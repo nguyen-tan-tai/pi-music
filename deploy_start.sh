7b3642440c2182b974fbe74dd8d0f06ff23db1b3
ERV_TaiNguyen <tai.nguyen@ever-rise.co.jp>
2021-08-20 00:13:48 +0900

    misc

--- deploy_start.sh
+++ deploy_start.sh
@@ -7,8 +7,8 @@ sudo rm -rf /var/www/html/*
 sudo cp -rf ~/pi-music/www/* /var/www/html/
 
 if [[ -f ~/pi-music/pidfile ]] ; then
-    kill $(cat ~/pi-music/pidfile) 2> /dev/null
-    rm ~/pi-music/pidfile 2> /dev/null
+    kill $(cat ~/pi-music/pidfile)
+    rm ~/pi-music/pidfile
 fi
 
 python3 ~/pi-music/src/pimusic.py
